41475632f15471116fabb917baf371421b56c429
Adrian Yong <ayong@zendesk.com>
2018-03-12 12:38:57 +1100

    sample files for pre-commit test

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+group :development do
+  gem 'byebug'
+  gem 'rubocop', '~> 0.52.1'
+end
---
+++ sample_ruby.rb
@@ -0,0 +1,15 @@
+class MyClass
+
+
+
+
+
+  def initialize
+  j
+
+  end 
+
+  def blah_this
+    puts 'blah'
+  end
+end
